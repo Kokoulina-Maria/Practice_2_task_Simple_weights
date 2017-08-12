04a650acb32becbf99cc2716a98dd56a70d395d4
Maria <Maria@LAPTOP-N156UH30>
2017-08-12 19:21:12 +0500

    Есть функция проверки, является ли число четным. Программа работает только в том случае, если число простое

--- Practice_2_task_Simple_weights/Program.cs
+++ Practice_2_task_Simple_weights/Program.cs
@@ -8,8 +8,24 @@ namespace Practice_2_task_Simple_weights
 {
     class Program
     {
+        static bool PrimeNumber(int number)
+        {//функция, определяющая, является ли данное число простым
+            for (int i=2; i<=Math.Sqrt(number); i++)//перебираем числа от 2 до корня из number
+            {
+                if (number % i == 0) return false;//если встрчаем число, на которое number делится, то возвращяем false
+            }
+            return true;//если не встречаем числа, на которое number делится, то возвращаем true
+        }
+
         static void Main(string[] args)
         {
+            int N = int.Parse(Console.ReadLine());//считываем N
+            if (PrimeNumber(N))
+            {
+                for (int i = 1; i <(N+1) / 2; i++)
+                    Console.WriteLine(i + " " + (N - i));
+            }
+            Console.ReadLine();
         }
     }
 }
